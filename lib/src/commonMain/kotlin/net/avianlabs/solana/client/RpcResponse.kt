2d386c1d487ca01d8ac97e5fe45e38e1b74ce190
Guillermo Orellana <wiyarmir@gmail.com>
2023-06-13 19:57:26 +0200

    rpc wrapper

--- lib/src/commonMain/kotlin/net/avianlabs/solana/client/RpcResponse.kt
+++ lib/src/commonMain/kotlin/net/avianlabs/solana/client/RpcResponse.kt
@@ -10,3 +10,16 @@ public data class RpcResponse(
   val result: JsonElement? = null,
   val error: RpcError? = null,
 )
+
+@Serializable
+public data class RPC<T>(
+  val context: Context?,
+  val value: T? = null,
+) {
+
+  @Serializable
+  public data class Context(
+    val slot: Long,
+    val apiVersion: String?,
+  )
+}
--- lib/src/commonMain/kotlin/net/avianlabs/solana/methods/getBalance.kt
+++ lib/src/commonMain/kotlin/net/avianlabs/solana/methods/getBalance.kt
@@ -1,18 +1,21 @@
 package net.avianlabs.solana.methods
 
 import kotlinx.serialization.json.JsonArray
+import kotlinx.serialization.json.decodeFromJsonElement
 import kotlinx.serialization.json.encodeToJsonElement
 import kotlinx.serialization.json.jsonObject
 import kotlinx.serialization.json.jsonPrimitive
 import kotlinx.serialization.json.long
 import net.avianlabs.solana.SolanaService
+import net.avianlabs.solana.client.RPC
 import net.avianlabs.solana.domain.PublicKey
 
 public suspend fun SolanaService.getBalance(
   account: PublicKey,
 ): Long {
   val result = invoke("getBalance", params(account))
-  return result?.jsonObject?.get("value")?.jsonPrimitive?.long!!
+  val rpc = json.decodeFromJsonElement<RPC<Long>>(result!!)
+  return rpc.value!!
 }
 
 private fun SolanaService.params(
