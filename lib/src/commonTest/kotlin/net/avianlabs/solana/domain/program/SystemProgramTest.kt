8e6c24378da410c68b30468ab74d4eebc8e34e3d
Guillermo Orellana <wiyarmir@gmail.com>
2023-06-16 18:27:46 +0200

    add test

---
+++ lib/src/commonTest/kotlin/net/avianlabs/solana/domain/program/SystemProgramTest.kt
@@ -0,0 +1,49 @@
+package net.avianlabs.solana.domain.program
+
+import io.ktor.util.*
+import kotlinx.coroutines.test.runTest
+import net.avianlabs.solana.SolanaClient
+import net.avianlabs.solana.client.RpcKtorClient
+import net.avianlabs.solana.crypto.Ed25519Keypair
+import net.avianlabs.solana.domain.core.Transaction
+import net.avianlabs.solana.methods.getMinimumBalanceForRentExemption
+import net.avianlabs.solana.methods.getRecentBlockhash
+import kotlin.test.Test
+
+class SystemProgramTest {
+
+  @Test
+  fun testCreateDurableNonceAccount() = runTest {
+    val client = SolanaClient(client = RpcKtorClient("https://api.devnet.solana.com"))
+
+    val keypair = Ed25519Keypair.fromBase58("")
+
+    val nonceAccount = Ed25519Keypair.fromBase58("")
+
+    val rentExempt = client.getMinimumBalanceForRentExemption(SystemProgram.NONCE_ACCOUNT_LENGTH)
+
+    val blockhash = client.getRecentBlockhash()
+
+    val transaction = Transaction()
+      .addInstruction(
+        SystemProgram.createAccount(
+          fromPublicKey = keypair.publicKey,
+          newAccountPublicKey = nonceAccount.publicKey,
+          lamports = rentExempt,
+          space = SystemProgram.NONCE_ACCOUNT_LENGTH,
+        )
+      )
+      .addInstruction(
+        SystemProgram.nonceInitialize(
+          nonceAccount = nonceAccount.publicKey,
+          authorized = keypair.publicKey,
+        )
+      )
+      .setRecentBlockHash(blockhash.blockhash)
+      .sign(listOf(keypair, nonceAccount))
+
+    val serialized = transaction.serialize().encodeBase64()
+
+    println(serialized)
+  }
+}
