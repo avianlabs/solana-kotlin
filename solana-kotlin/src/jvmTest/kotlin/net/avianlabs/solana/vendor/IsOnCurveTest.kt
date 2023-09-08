a55045a8dc64444f46228b3390f37af5a789bc88
Guillermo Orellana <wiyarmir@gmail.com>
2023-09-08 13:02:27 +0200

    vendor sha256 and move working arrays into the function call to make them thread safe

--- solana-kotlin/src/jvmTest/kotlin/net/avianlabs/solana/vendor/IsOnCurveTest.kt
+++ solana-kotlin/src/jvmTest/kotlin/net/avianlabs/solana/vendor/IsOnCurveTest.kt
@@ -1,9 +1,15 @@
 package net.avianlabs.solana.vendor
 
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.runBlocking
 import net.avianlabs.solana.crypto.defaultCryptoEngine
 import net.avianlabs.solana.crypto.isOnCurve
 import net.avianlabs.solana.domain.core.PublicKey
+import net.avianlabs.solana.domain.program.ProgramDerivedAddress
+import net.avianlabs.solana.domain.program.associatedTokenAddress
 import org.junit.Test
+import kotlin.test.assertEquals
 import kotlin.test.assertFalse
 import kotlin.test.assertTrue
 
@@ -15,5 +21,28 @@ class IsOnCurveTest {
 
     val onCurve = defaultCryptoEngine.generateKey().publicKey
     assertTrue(onCurve.isOnCurve())
+  }
+
+  @Test
+  fun testParallelAssociatedAddress() {
+    runBlocking {
+      val x = PublicKey.fromBase58("4rZoSK72jVaAW1ayZLrefdMPAAStRVhCfH1PSundaoNt")
+      val y = PublicKey.fromBase58("EPjFWdd5AufqSSqeM2qN1xzybapC8G4wEGGkZwyTDt1v")
+      val dx = ProgramDerivedAddress(PublicKey.fromBase58("BKN4phaq8HCe5bensWGCsgZoJ1JM3LGwbMP5FesbYY37"), 251u)
+      launch {
+        this.launch(Dispatchers.IO) {
+          for (i in 0..100) {
+            val result = x.associatedTokenAddress(y)
+            assertEquals(dx, result)
+          }
+        }
+        this.launch(Dispatchers.IO) {
+          for (i in 0..100) {
+            val result = x.associatedTokenAddress(y)
+            assertEquals(dx, result)
+          }
+        }
+      }
+    }
   }
 }
---
+++ solana-kotlin/src/jvmTest/kotlin/net/avianlabs/solana/vendor/Sha256Test.kt
@@ -0,0 +1,32 @@
+package net.avianlabs.solana.vendor
+
+import io.ktor.util.decodeBase64Bytes
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.runBlocking
+import org.komputing.khash.sha256.Sha256
+import kotlin.test.Test
+
+val string = "Hello, world!".encodeToByteArray()
+val expected = "MV9b23bQeMQ7isAGTkoBZGErH853yGk0W/yUx1iU7dM=".decodeBase64Bytes()
+
+class Sha256Test {
+
+  @Test
+  fun testParallelSha256() {
+    runBlocking {
+      launch(Dispatchers.Unconfined) {
+        repeat(10000) {
+          val actual = Sha256.digest(string)
+          assert(expected.contentEquals(actual))
+        }
+      }
+      launch(Dispatchers.Unconfined) {
+        repeat(10000) {
+          val actual = Sha256.digest(string)
+          assert(expected.contentEquals(actual))
+        }
+      }
+    }
+  }
+}
