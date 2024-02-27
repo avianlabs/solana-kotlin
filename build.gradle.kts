0c14b56fc7421fa46539847f09b3990eb990f521
Guillermo Orellana <guillermo@avianlabs.net>
2024-02-27 15:04:52 +0000

    use nmcp

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,10 +1,19 @@
 plugins {
   alias(libs.plugins.kotlinMultiplatform).apply(false)
   alias(libs.plugins.androidLib).apply(false)
+  alias(libs.plugins.nmcp)
 }
 
 if (rootProject.findProperty("snapshot") != "false") {
   allprojects {
     version = "$version-SNAPSHOT"
+  }
+}
+
+nmcp {
+  publishAllProjectsProbablyBreakingProjectIsolation {
+    username = findProperty("mavenCentralUsername") as String
+    password = findProperty("mavenCentralPassword") as String
+    publicationType = "USER_MANAGED"
   }
 }
