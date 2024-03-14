4b93b628140c831490f158abc950b37c4ef2df8b
Guillermo Orellana <guillermo@avianlabs.net>
2024-03-14 14:05:29 +0000

    no checksum

--- Package.swift
+++ Package.swift
@@ -15,8 +15,7 @@ let package = Package(
     targets: [
         .binaryTarget(
             name: "SolanaKotlin",
-            url: "https://github.com/avianlabs/solana-kotlin/releases/download/0.1.5/SolanaKotlin.zip",
-            checksum: "bf56d2279c0b633ae80fd68110136bd1e7ae6862e95bbc676bbe255752ff4fd9"
+            url: "https://github.com/avianlabs/solana-kotlin/releases/download/0.1.5/SolanaKotlin.zip"
         ),
     ]
 )
