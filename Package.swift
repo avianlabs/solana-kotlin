e5ca5acbb0f589d0bfa075c79b926b7506212916
Guillermo Orellana <guillermo@avianlabs.net>
2024-03-15 09:58:43 +0000

    new version

--- Package.swift
+++ Package.swift
@@ -16,7 +16,7 @@ let package = Package(
         .binaryTarget(
             name: "SolanaKotlin",
             url: "https://github.com/avianlabs/solana-kotlin/releases/download/0.1.5/SolanaKotlin.zip",
-            checksum: "15f0154a6407f683eeab50825ac52887b71258e46e4984f68d7646d952d1f153"
+            checksum: "715f41796ff56c7270451f66c2372a747768a15b514ccdb5afd3652b33ec3781"
         ),
     ]
 )
