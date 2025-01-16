6008c275c394e293d0cc0100cd129ed048a2992c
Guillermo Orellana <guillermo@avianlabs.net>
2025-01-16 13:43:26 +0100

    wip swiftexport

--- solana-kotlin/build.gradle.kts
+++ solana-kotlin/build.gradle.kts
@@ -1,5 +1,4 @@
-import co.touchlab.skie.configuration.ClassInterop
-import co.touchlab.skie.configuration.DefaultArgumentInterop
+import org.jetbrains.kotlin.gradle.swiftexport.ExperimentalSwiftExportDsl
 
 plugins {
   alias(libs.plugins.kotlinMultiplatform)
@@ -8,14 +7,13 @@ plugins {
   alias(libs.plugins.dokka)
   signing
   alias(libs.plugins.multiplatform.swiftpackage)
-  alias(libs.plugins.skie)
 }
 
 group = "net.avianlabs.solana"
 version = properties["version"] as String
 
 kotlin {
-  targetHierarchy.default()
+  applyDefaultHierarchyTemplate()
   explicitApi()
 
   jvm {
@@ -27,19 +25,34 @@ kotlin {
     }
   }
 
-  listOf(
-    iosArm64(),
-    iosSimulatorArm64(),
-  ).forEach { iosTarget ->
-    iosTarget.binaries.framework {
-      baseName = "SolanaKotlin"
-      export(project(":tweetnacl-multiplatform"))
-      isStatic = true
-    }
-  }
+  iosArm64()
+  iosSimulatorArm64()
 
   mingwX64()
   linuxX64()
+
+  @OptIn(ExperimentalSwiftExportDsl::class)
+  swiftExport {
+    // Root module name
+    moduleName = "SolanaKotlin"
+
+    // Collapse rule
+    flattenPackage = "net.avianlabs.solana"
+
+//    // Export external modules
+//    export(project(":tweetnacl-multiplatform")) {
+//      // Exported module name
+//      moduleName = "TweetNaCl"
+//      // Collapse exported dependency rule
+//      flattenPackage = "net.avianlabs.solana.tweetnacl"
+//    }
+    export(libs.ktorClientCore)
+//    export(libs.ktorClientDarwin)
+    export(libs.ktorHttp)
+    export(libs.ktorEvents)
+    export(libs.ktorIo)
+    export(libs.coroutinesCore)
+  }
 
   sourceSets {
     val jvmMain by getting {
@@ -97,15 +110,6 @@ kotlin {
   }
 }
 
-skie {
-  features {
-    group("net.avianlabs.solana.tweetnacl") {
-      ClassInterop.CInteropFrameworkName("TweetNaClMultiplatform")
-      DefaultArgumentInterop.Enabled(true)
-    }
-  }
-}
-
 multiplatformSwiftPackage {
   swiftToolsVersion("5.9")
   targetPlatforms {
@@ -140,6 +144,7 @@ publishing {
       }
     }
   }
+
   publications {
     withType<MavenPublication> {
       pom {
