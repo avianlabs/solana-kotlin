6b7f36f7c8314c36fdfa32e8aea6df9cc68dc13c
wiyarmir <wiyarmir@users.noreply.github.com>
2026-02-27 17:15:32 +0000

    Commit from GitHub Actions (Release Swift package)

--- Package.swift
+++ Package.swift
@@ -1,10 +1,10 @@
-// swift-tools-version:5.9
+// swift-tools-version:5.10
 import PackageDescription
 
 let package = Package(
     name: "SolanaKotlin",
     platforms: [
-        .iOS(.v16)
+        .iOS(.v17)
     ],
     products: [
         .library(
@@ -15,8 +15,8 @@ let package = Package(
     targets: [
         .binaryTarget(
             name: "SolanaKotlin",
-            url: "https://github.com/avianlabs/solana-kotlin/releases/download/0.4.2/SolanaKotlin.zip",
-            checksum: "ed2545ac01d5608adf7892da93c35bebbbafc26f1acd75ef83b9f99f4789c14c"
+            url: "https://github.com/avianlabs/solana-kotlin/releases/download/0.4.3/SolanaKotlin.zip",
+            checksum: "e06c36b50687a656b1e660a5ac3df9fc5c7bc03dd222d359a3a0f9c5f0a9233d"
         ),
     ]
 )
