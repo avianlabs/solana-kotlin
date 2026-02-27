6d607c1ed80efc4e9aaa2a93896302fd1a9aa7a4
waliahimanshu <waliahimanshu@users.noreply.github.com>
2026-02-27 13:35:40 +0000

    Commit from GitHub Actions (Release Swift package)

--- Package.swift
+++ Package.swift
@@ -15,8 +15,8 @@ let package = Package(
     targets: [
         .binaryTarget(
             name: "SolanaKotlin",
-            url: "https://github.com/avianlabs/solana-kotlin/releases/download/0.4.1/SolanaKotlin.zip",
-            checksum: "0b2c097605d80f6f99d32f798585001d687e0fef09f886cab68f1710ad79432b"
+            url: "https://github.com/avianlabs/solana-kotlin/releases/download/0.4.2/SolanaKotlin.zip",
+            checksum: "ed2545ac01d5608adf7892da93c35bebbbafc26f1acd75ef83b9f99f4789c14c"
         ),
     ]
 )
