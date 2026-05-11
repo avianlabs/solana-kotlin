0c73ab1d7b7d312c466ef192bc5872584cc6fe15
wiyarmir <172084+wiyarmir@users.noreply.github.com>
2026-05-11 04:26:28 +0000

    update Solana program IDLs and generated code

--- solana-kotlin/src/commonMain/generated/net/avianlabs/solana/domain/program/Token2022Program.kt
+++ solana-kotlin/src/commonMain/generated/net/avianlabs/solana/domain/program/Token2022Program.kt
@@ -1773,7 +1773,7 @@ public object Token2022Program : TokenProgram() {
    * owned account by sending them to any other account, leaving behind only
    * lamports for rent exemption.
    */
-  public fun withdrawExcessLamports(
+  public override fun withdrawExcessLamports(
     sourceAccount: PublicKey,
     destinationAccount: PublicKey,
     authority: PublicKey,
@@ -2350,7 +2350,7 @@ public object Token2022Program : TokenProgram() {
   /**
    * Transfer lamports from a native SOL account to a destination account.
    */
-  public fun unwrapLamports(
+  public override fun unwrapLamports(
     source: PublicKey,
     destination: PublicKey,
     authority: PublicKey,
